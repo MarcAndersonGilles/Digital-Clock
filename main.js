a2410ec5c731c23794a1718160c07aa3240190db
marc <hornil6748@gmail.com>
2023-10-01 13:32:03 -0400

    digital clock

--- main.js
+++ main.js
@@ -2,22 +2,24 @@ const htmlHours = document.querySelector('.hours')
 const htmlMinutes = document.querySelector('.minutes')
 const htmlSeconds = document.querySelector('.seconds')
 const htmlAmpPm = document.querySelector('.ampm')
+const htmlDate = document.querySelector('.dateTime')
 
 const getTime = () => {
-  const nowTime = new Date()
-  
+  const nowTime = new Date();
 
-  const hours = nowTime.getHours()
-  const minutes = nowTime.getMinutes()
-  const seconds = nowTime.getSeconds()
-  
-  
- const ampm = hours >=12 ? "PM" : "AM";
+  const hours = nowTime.getHours();
+  const minutes = nowTime.getMinutes();
+  const seconds = nowTime.getSeconds();
 
-  htmlHours.textContent = hours
-  htmlMinutes.textContent = minutes
-  htmlSeconds.textContent = seconds
-  htmlAmpPm.textContent = ampm
-  
+  const date = nowTime.toDateString();
+
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  htmlHours.textContent = hours;
+  htmlMinutes.textContent = minutes;
+  htmlSeconds.textContent = seconds;
+  htmlAmpPm.textContent = ampm;
+  htmlDate.textContent = date;
+
 }
-setInterval(getTime, 1000)
+setInterval(getTime, 1000);
