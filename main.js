b97153d343f2ce62477f3e10580b1bf3cd4d8a9c
marc <hornil6748@gmail.com>
2023-10-01 13:06:34 -0400

    Ajouter si c'est pm ou am

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const htmlHours = document.querySelector('.hours')
 const htmlMinutes = document.querySelector('.minutes')
 const htmlSeconds = document.querySelector('.seconds')
+const htmlAmpPm = document.querySelector('.ampm')
 
 const getTime = () => {
   const nowTime = new Date()
@@ -9,12 +10,14 @@ const getTime = () => {
   const hours = nowTime.getHours()
   const minutes = nowTime.getMinutes()
   const seconds = nowTime.getSeconds()
+  
+  
+ const ampm = hours >=12 ? "PM" : "AM";
 
   htmlHours.textContent = hours
   htmlMinutes.textContent = minutes
   htmlSeconds.textContent = seconds
-  console.log(hours)
-  console.log(minutes)
-  console.log(seconds)
+  htmlAmpPm.textContent = ampm
+  
 }
 setInterval(getTime, 1000)
